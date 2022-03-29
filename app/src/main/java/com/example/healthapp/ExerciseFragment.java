e031d7a9987ccf7b7fb72464f8f01177f928ff16
Benjamin Cai <bcai@Benjamins-MacBook-Air.local>
2022-03-29 13:04:43 -0400

    excercise changes

--- app/src/main/java/com/example/healthapp/ExerciseFragment.java
+++ app/src/main/java/com/example/healthapp/ExerciseFragment.java
@@ -1,5 +1,7 @@
 package com.example.healthapp;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +9,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ExerciseFragment extends Fragment {
+
+    private AutoCompleteTextView addExcercise;
+    private Button addOtherExcerciseButton;
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private EditText excerciseName, calories, length;
+    private Button addOtherExcerciseSave, addOtherExcerciseCancel;
+
+    View view;
+
+    public String[] workouts = {
+            "Running",
+            "Weight Lifting",
+            "Walk",
+            "Climbing",
+            "Hiking",
+            "Jogging"
+    };
 
     public ExerciseFragment() {
         // Required empty public constructor
@@ -18,6 +43,26 @@ public class ExerciseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise, container, false);
+
+        view = inflater.inflate(R.layout.fragment_exercise, container, false);
+
+        addExcercise = (AutoCompleteTextView) view.findViewById(R.id.addExcerciseItem);
+        //Creating the instance of ArrayAdapter containing list of fruit names
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (getActivity(), android.R.layout.select_dialog_item, workouts);
+
+        //Getting the instance of AutoCompleteTextView
+        addExcercise.setThreshold(1);//will start working from first character
+        addExcercise.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+        addExcercise.setTextColor(Color.RED);
+
+
+        return view;
+
+
+
+
+
+
     }
 }
