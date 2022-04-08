3558ab04bca724a6b36d911dcd9ffc00701b0fad
Erik Hillard Laptop <erikhillard2018@gmail.com>
2022-04-08 16:11:04 -0400

    Cleaned up some comments

--- app/src/main/java/com/example/healthapp/LoginActivity.java
+++ app/src/main/java/com/example/healthapp/LoginActivity.java
@@ -17,25 +17,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        TextView username = findViewById(R.id.usernameInput);
-//        TextView password = findViewById(R.id.passwordInput);
-//
-//        MaterialButton loginButton = (MaterialButton) findViewById(R.id.loginbutton);
-//
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (username.getText().toString().equals("user")
-//                        && password.getText().toString().equals("pass")){
-//                    Intent switchActivity = new Intent(LoginActivity.this, MainActivity.class);
-//                    switchActivity.putExtra("Username", username.getText().toString());
-//                    switchActivity.putExtra("NewUser", false);
-//                    startActivity(switchActivity);
-//                } else {
-//                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
